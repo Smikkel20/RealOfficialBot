0a50d74878df3e73771ee9af8666637378bc1421
Smikkel20 <43788333+Smikkel20@users.noreply.github.com>
2021-04-15 17:48:31 +0200

    Create help.py
    added a help command

---
+++ cogs/help.py
@@ -0,0 +1,22 @@
+import discord
+import os
+import random
+from discord.ext.commands import Bot
+from discord.ext import commands
+
+class Helpcommand(commands.Cog):
+    
+    def __init__(self, bot, *args, **kwargs):
+        self.bot = bot
+    
+    #Creates subcommand for help
+    @commands.command()
+    async def help(self, ctx):
+        em = discord.Embed(title = "Help", color = discord.Color.blue())
+        em.add_field(name = "`;sw`", value = "use `;sw`, `;starwars` for starwars quotes")
+        em.add_field(name = "`;q`", value = "use `;q`, `;quotes` for official sexy quotes")
+        await ctx.send(embed = em)
+
+def setup(bot):
+    bot.add_cog(Helpcommand(bot))
+    print("Help has been loaded")
