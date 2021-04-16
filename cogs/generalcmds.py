c48d1165dde21a318fdd7294f53d52bff976dc2e
Smikkel20 <43788333+Smikkel20@users.noreply.github.com>
2021-04-16 19:12:25 +0200

    Update generalcmds.py
    added bans to see how many bans everyone has

--- cogs/generalcmds.py
+++ cogs/generalcmds.py
@@ -2,6 +2,9 @@ import discord
 import os
 import random
 import json
+import requests
+import io
+from bs4 import BeautifulSoup
 from discord.ext.commands import Bot
 from discord.ext import commands
 
@@ -25,6 +28,55 @@ class cmds(commands.Cog):
     
     def __init__(self, bot, *args, **kwargs):
         self.bot = bot
+
+    @commands.command(aliases = ["bans"])
+    async def _bans(self,ctx):
+        URL = "https://docs.google.com/document/d/1IqqL4FtKwvp9mmO2QAljmAGeadSCI2bu1C7W-OSXwig/edit"
+        page = requests.get(URL)
+
+        soup = BeautifulSoup(page.content, 'html.parser')
+        results = soup.find_all("script", type='text/javascript')
+
+        with io.open("html.txt", "w", encoding="utf-8") as f:
+            f.write(str(results))
+            f.close
+
+        f = io.open("html.txt", "r", encoding="utf-8")
+        File = str(f.read())
+        people = []
+        number = 0
+
+        for i in File.split("u001c"):
+            number += 1
+            print(number)
+            if number % 2 == 0:
+                print(i)
+                i = i[:-3]
+                people.append(i)
+            else:
+                i = i[:-9]
+                people.append(i)
+
+        people = people[1:]
+        last = people[-1]
+        number = 0
+
+        for i in last.split("n"):
+            if number == 0:
+                people[-1] = i[:-1]
+            number += 1
+
+        number = 0
+        em = discord.Embed(title="BANS",color=discord.Color.blue())
+        for i in people:
+            number += 1
+            if number % 2 == 0:
+                Number = i
+                em.add_field(name= name ,value= f"{name} - {Number}", inline=False)
+            else:
+                name = i
+        await ctx.send(embed = em)        
+            
 
     @commands.command(aliases = ["swquotes", "starwars", "starwarsquotes", "sw", "swq"])
     async def _sw(self, ctx):
