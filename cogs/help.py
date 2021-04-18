1b8ce8150bba155811d834999fb9e16cc9753613
Smikkel20 <43788333+Smikkel20@users.noreply.github.com>
2021-04-18 11:32:09 +0200

    Update help.py
    added 3 commands to help

--- cogs/help.py
+++ cogs/help.py
@@ -13,8 +13,11 @@ class Helpcommand(commands.Cog):
     @commands.command()
     async def help(self, ctx):
         em = discord.Embed(title = "Help", color = discord.Color.blue())
+        em.add_field(name = "`;bible`", value = "use `;bible` to get a link to the official bible", inline=False)             
         em.add_field(name = "`;sw`", value = "use `;sw`, `;starwars` for starwars quotes", inline=False)
         em.add_field(name = "`;q`", value = "use `;q`, `;quotes` for official sexy quotes", inline=False)
+        em.add_field(name = "`;bans`", value = "use `;bans` to see how many bans everyone has!", inline=False)
+        em.add_field(name = "`;banaan`", value = "use `;banaan`, `;bananen` to get photos of a beautyfull banana!", inline=False)
         await ctx.send(embed = em)
 
 def setup(bot):
